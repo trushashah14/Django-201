9013f5bf53c33fe102628092c096a6055b7ad865
Trusha Shah <trushashah1402@gmail.com>
2024-04-10 17:14:20 +0530

    Add detail view but not a template

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,4 @@
-from django.views.generic import ListView
-
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class HomePage(ListView):
@@ -9,3 +8,8 @@ class HomePage(ListView):
     context_object_name= "posts"
     queryset = Post.objects.all().order_by('-id')[0:30]
 
+class PostDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name ="feed/detail.html"
+    model = Post
+    context_object_name = "post"
