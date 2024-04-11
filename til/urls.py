444a35709daa13efc1b18f61506602d7689a50d1
Trusha Shah <trushashah1402@gmail.com>
2024-04-11 11:21:43 +0530

    Adding a Profile Detail View

--- til/urls.py
+++ til/urls.py
@@ -20,11 +20,13 @@ from django.urls import path
 from feed import urls as feed_urls
 from django.conf.urls.static import static
 from django.conf import settings
+from profiles import urls as profiles_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(feed_urls, namespace="feed")),
+    path("profile/", include(profiles_urls, namespace="profiles")),
     path("", include("allauth.urls")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
