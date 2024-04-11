fe94aa997b78d14f7e8d791801a5981aaf69f48a
Trusha Shah <trushashah1402@gmail.com>
2024-04-11 11:08:50 +0530

    Enabling static files

--- til/settings.py
+++ til/settings.py
@@ -159,4 +159,10 @@ AUTHENTICATION_BACKENDS = (
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "frontend/")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
